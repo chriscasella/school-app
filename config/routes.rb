9228452e7b1e2dc97aa3397f9721a3319704c56a
chris.casella <chris.casella@gmail.com>
2017-07-19 18:58:39 -0400

    Added and setup Devise with migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :students
+    resources :assignments
+  devise_for :schools
+  devise_for :teachers do
+    resources :classes do
+      resources :assignments
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
