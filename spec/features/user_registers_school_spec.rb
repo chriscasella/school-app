28d488cc363e652d8df2c06232210d03790f147d
chris.casella <chris.casella@gmail.com>
2017-08-01 21:45:20 -0400

    New school page signup added fixed test as well

--- spec/features/user_registers_school_spec.rb
+++ spec/features/user_registers_school_spec.rb
@@ -4,6 +4,6 @@ feature "user registers school" do
   scenario do
     visit new_school_registration_path
     fill_in 'School Name', with: 'Test High School'
-    click_link 'Register New School'
+    click_button 'Register New School'
   end
 end
