c5a655f42f9b055c5a0a7c3cd4787957121553d2
chris.casella <chris.casella@gmail.com>
2017-08-02 11:38:17 -0400

    Validations corrected for Teachers

--- app/models/school.rb
+++ app/models/school.rb
@@ -4,7 +4,4 @@ class School < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :school_id, presence: true, numericality: true
 end
