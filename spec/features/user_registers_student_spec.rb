01125f2f6f15661e12bb22681d02f5166c53b837
chris.casella <chris.casella@gmail.com>
2017-08-03 10:13:19 -0400

    Student test

---
+++ spec/features/user_registers_student_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature "user registers student" do
+  scenario do
+    visit new_student_registration_path
+    fill_in 'First Name', with: 'Joe'
+    fill_in 'Last Name', with: 'Gurdy'
+    fill_in 'School ID', with: 1
+    fill_in 'Email', with: 'joe.gurdy@gmail.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+  end
+end
